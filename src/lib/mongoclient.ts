d9479c3014221ad60dacb776348b98a91959963b
Anish Acharya <acharyaanish920@gmail.com>
2025-03-25 23:13:46 +0545

    refactor: update MongoDB connection handling and improve error messaging

--- src/lib/mongoclient.ts
+++ src/lib/mongoclient.ts
@@ -1,32 +1,32 @@
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URL) {
-  throw new Error("Please add your MongoDB URI to .env.local");
+if (!process.env.MONGODB_URI) {
+  throw new Error("Invalid/Missing environment variable: 'MONGODB_URI'");
 }
 
-const uri = process.env.MONGODB_URL;
-
+const uri: string = process.env.MONGODB_URI;
 const options = {};
 
-let client;
-let clientPromise;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 declare global {
+  // allow global `var` declarations
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (process.env.NODE_ENV === "development") {
-  // In development, use a global variable so the connection is cached.
+  // In development mode, use a global variable to preserve value across module reloads caused by HMR.
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise as Promise<MongoClient>;
+  clientPromise = global._mongoClientPromise;
 } else {
-  // In production, it's best not to use a global variable.
+  // In production mode, it's best not to use a global variable.
   client = new MongoClient(uri, options);
-  clientPromise = client.connect() as Promise<MongoClient>;
+  clientPromise = client.connect();
 }
 
 export default clientPromise;
